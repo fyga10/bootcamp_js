28cddecb87d3a71a26b7e8d5e17a9104605acf99
fyga10 <frankgiraldo62@outlook.com>
2022-08-15 12:26:26 -0500

    my first project in open_bootcamp

---
+++ mi_primer_proyecto_js/index.js
@@ -0,0 +1,11 @@
+// este es el mensaje de bienvenida
+
+console.log("esta es la puerta de entrada al proyecto")
+
+// esta es la parte intermedia del proyecto
+
+
+
+// este es el mensaje para despedirse
+
+console.log("adios")
